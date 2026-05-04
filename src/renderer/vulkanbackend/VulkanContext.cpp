6deb613370f85cdb68c88ba6b19ba7f9b06a7092
Unknown <christian-rumpke@web.de>
2026-05-04 20:30:31 +0200

    glfw

---
+++ src/renderer/vulkanbackend/VulkanContext.cpp
@@ -0,0 +1,3 @@
+//
+// Created by christian on 04.05.26.
+//
---
+++ src/renderer/vulkanbackend/VulkanContext.h
@@ -0,0 +1,8 @@
+//
+// Created by christian on 04.05.26.
+//
+
+#ifndef IHS_VULKANCONTEXT_H
+#define IHS_VULKANCONTEXT_H
+
+#endif //IHS_VULKANCONTEXT_H
