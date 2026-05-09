2dbf1624f698901014480e25b32759cf2896f136
Unknown <christian-rumpke@web.de>
2026-05-09 13:05:14 +0200

    Swapchain implemented

---
+++ src/renderer/vulkanbackend/VulkanSwapchain.cpp
@@ -0,0 +1,3 @@
+//
+// Created by christian on 09.05.26.
+//
---
+++ src/renderer/vulkanbackend/VulkanSwapchain.h
@@ -0,0 +1,8 @@
+//
+// Created by christian on 09.05.26.
+//
+
+#ifndef IHS_VULKANSWAPCHAIN_H
+#define IHS_VULKANSWAPCHAIN_H
+
+#endif //IHS_VULKANSWAPCHAIN_H
