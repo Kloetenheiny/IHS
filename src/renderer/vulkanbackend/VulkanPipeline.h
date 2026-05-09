8b5b7d12033bd6d71b0a9444088c751c99420a0d
Unknown <christian-rumpke@web.de>
2026-05-09 17:39:05 +0200

    .

---
+++ src/renderer/vulkanbackend/VulkanPipeline.h
@@ -0,0 +1,8 @@
+//
+// Created by christian on 09.05.26.
+//
+
+#ifndef IHS_VULKANPIPELINE_H
+#define IHS_VULKANPIPELINE_H
+
+#endif //IHS_VULKANPIPELINE_H
---
+++ src/renderer/vulkanbackend/VulkanRenderer.h
@@ -0,0 +1,8 @@
+//
+// Created by christian on 09.05.26.
+//
+
+#ifndef IHS_VULKANRENDERER_H
+#define IHS_VULKANRENDERER_H
+
+#endif //IHS_VULKANRENDERER_H
